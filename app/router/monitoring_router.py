da04adb54f64af1bbed0520ddd2ce4ed22c2c9c1
Teddy Dee <tkzteddy@gmail.com>
2022-03-01 15:49:40 +0700

    Add Monitoring History Pasien

--- app/router/monitoring_router.py
+++ app/router/monitoring_router.py
@@ -10,7 +10,7 @@ def monitoring_history_pelayanan():
     no_kartu = request.args.get('no_kartu')
     tgl_mulai = request.args.get('tgl_mulai')
     tgl_akhir = request.args.get('tgl_akhir')
-    end_point = 'monitoring/HistoriPelayanan/NoKartu/'+no_kartu+'/tglMulai/'+tgl_mulai+'/tglAkhir'+tgl_akhir
+    end_point = 'monitoring/HistoriPelayanan/NoKartu/'+no_kartu+'/tglMulai/'+tgl_mulai+'/tglAkhir/'+tgl_akhir
     method = 'get'
     payload = ''
     return BpjsController.bridging(end_point,method,payload)
